88eddedaa2ed1c72e85c293158a782a587a4ab0a
davidjeffrey058 <113363460+davidjeffrey058@users.noreply.github.com>
2023-04-04 12:44:08 +0000

    Updated the authentication page

--- Scripts/mainLayout.js
+++ Scripts/mainLayout.js
@@ -1,23 +1,82 @@
-var items =  [];
-const setUpTasksLayout = (data) => {
 
+const setUpTasksLayout = (data, email) => {
+    var items = [];
 
     data.forEach(element => {
-        items.push(element.data());
+        items.push({
+            id: element.id,
+            ...element.data()
+        });
     });
+    console.log(items);
 
     let container = '';
     var i = 0;
     items.forEach(docs => {
         const li = `
-    <li>
-        <div><b>Content: </b>${items[i].text}</div>
-        <div><b>Category: </b>${items[i].category}</div>
-        <div><b>Status: </b>${items[i].status}</div><br>
-    </li>
+        <div class="item_container">
+            <div data-id="${items[i].id}" class="check-mark ${items[i].status == "completed" ? "checked":""}">
+                <img src="images/icon-check.svg">
+            </div>
+          <p class="task_name ${items[i].status == "completed" ? "checked":""}">${items[i].text}</p>
+          <div data-id="${items[i].id}" class="delete-text ${items[i].status == "completed" ? "checked":""}">Delete</div>
+        </div>
     `;
         container += li;
         i++;
     });
-    document.querySelector('.tasks').innerHTML = container
+    document.querySelector('.theContainer').innerHTML = container;
+    createEventListeners(email);
+    createEventListenersDelete(email);
+}
+
+//Creates an eventListener for every single checkbox
+function createEventListeners(Email){
+    var todoCheckMarks = document.querySelectorAll('.item_container .check-mark');
+
+    todoCheckMarks.forEach((checkMark) => {
+        checkMark.addEventListener("click", function(){
+            markCompleted(checkMark.dataset.id, Email);
+        });
+    });
+}
+
+//Creates an eventListener for every single delete
+function createEventListenersDelete(Email){
+    var todoCheckMarks = document.querySelectorAll('.item_container .delete-text');
+
+    todoCheckMarks.forEach((checkMark) => {
+        checkMark.addEventListener("click", function(){
+            deleteTask(checkMark.dataset.id, Email);
+        });
+    });
+}
+
+//Function that fires when a checkbox is clicked
+function markCompleted(id, e){
+    let item = db.collection(e).doc(id);
+    item.get().then(function(doc){
+        if(doc.exists){
+            let status = doc.data().status;
+            if(status == "active"){
+                item.update({
+                    status: "completed"
+                })
+            }else if(status == "completed"){
+                item.update({
+                    status: "active"
+                })
+            }
+        }
+    });
+}
+
+//Function that deletes a task
+function deleteTask(id, e){
+    let item = db.collection(e).doc(id);
+    item.delete().then(()=>{
+        alert("Deleted successfully");
+    }).catch(error => {
+        alert("Error deleting document: ", error);
+    })
 }
--- Scripts/mainfirebase.js
+++ Scripts/mainfirebase.js
@@ -1,18 +1,23 @@
 var userId = '';
 var userEmail = '';
 
+
 //Tracks the authenticaiton state
 auth.onAuthStateChanged(user => {
   if (user) {
+    const userName = document.querySelector('.displayName');
     userEmail = user.email;
     userId = user.uid;
 
+    userName.innerHTML = 'Hi ' +  firstName(user.displayName);
+    userName.title = 'Logged in as ' + user.email;
+
     //get all the tasks from the users collection
     db.collection(userEmail).onSnapshot(snapshot => {
-      setUpTasksLayout(snapshot.docs);
+      setUpTasksLayout(snapshot.docs, userEmail);
     });
-    console.log(user.email);
-    document.querySelector('#userEmail').innerHTML = 'Logged in as ' + user.email
+    // console.log(user);
+    //document.querySelector('#userEmail').innerHTML = 'Logged in as ' + user.email
   }
   else {
     window.location.href = './index.html';
@@ -30,23 +35,29 @@ function addItem(e) {
   e.preventDefault();
   const taskText = document.querySelector('.addTask');
 
-  if (items.length == 0) {
-    db.collection(userEmail).doc().set({
-      text: taskText['add_task_field'].value,
-      status: 'active',
-      category: taskText['categorySelect'].value
-    }).then(() => {
-      taskText.reset();
-    })
-  } else {
-    items.length = 0;
+  // if (items.length == 0) {
+  //   db.collection(userEmail).doc().set({
+  //     text: taskText['task_input'].value,
+  //     status: 'active',
+  //     category: taskText['categorySelect'].value
+  //   }).then(() => {
+  //     taskText.reset();
+  //   })
+  // } else {
+    // items.length = 0;
     db.collection(userEmail).add({
-      text: taskText['add_task_field'].value,
+      text: taskText['task_input'].value,
       status: 'active',
       category: taskText['categorySelect'].value
     }).then(() => {
       taskText.reset();
     })
-  }
+  // }
+}
+
+//method that splits the fullname and returns the first name
+function firstName(value){
+  var result = value.split(" ");
+  return result[0];
 }
 
