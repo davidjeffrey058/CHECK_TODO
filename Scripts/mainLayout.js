e92a2ae9dda675a60259bc9643a391ffdaf05194
davidjeffrey058 <113363460+davidjeffrey058@users.noreply.github.com>
2023-04-29 04:56:55 +0000

    may revert

--- Scripts/mainLayout.js
+++ Scripts/mainLayout.js
@@ -9,8 +9,9 @@ const setUpTasksLayout = (data, email) => {
             ...element.data()
         });
     });
-    
-    document.querySelector('.theContainer').innerHTML = todoLayout(items);
+
+    document.querySelector('.theContainer').innerHTML = todoLayout(items, "active");
+    document.querySelector('.doneContainer').innerHTML = todoLayout(items, "completed");
     document.querySelector(".collab").innerHTML = todoLayout(items);
 
     //Copying the important items into the important list
@@ -118,30 +119,92 @@ function formateDate(dateValue) {
 }
 
 
-function todoLayout(items) {
+function todoLayout(items, status) {
     let container = '';
-    items.forEach(docs => {
-        const li = `
-    <div class="item_container" title="Created on: ${formateDate(docs.createdDate)}">
-        <div data-id="${docs.id}" class="check-mark ${docs.status == "completed" ? "checked" : ""}">
-            <img src="images/icon-check.svg">
+    if (status == null) {
+        items.forEach(docs => {
+            const li = `
+         <div class="item_container" title="Created on: ${formateDate(docs.createdDate)}">
+             <div data-id="${docs.id}" class="check-mark ${docs.status == "completed" ? "checked" : ""}">
+                 <img src="images/icon-check.svg">
+             </div>
+             <div class="text-container">
+                 <p class="task_name ${docs.status == "completed" ? "checked" : ""}">${docs.text}</p>
+                 <div>
+                     <label class="task_category">${docs.category}</label> 
+                     <label class="task_endDate ${docs.endDate == "" ? "empty" : ""}"><i>Due in: ${timeLeft(docs.createdDate, docs.endDate)}</i></label>
+                 </div>           
+             </div>
+           <div class="star-container ${docs.important == true ? "important" : ""}" data-id="${docs.id}" title="Mark as important">
+             <i class="fa-solid fa-star"></i>
+           </div>
+           <div title="Delete task" data-id="${docs.id}" class="delete-text ${docs.status == "completed" ? "checked" : ""}">
+             <i class="fa-regular fa-trash-can fa-bounce fa-xl" style="color: #ff0000;"></i>
+           </div>
+         </div>
+     `;
+            container += li;
+        });
+    }else{
+        items.forEach(docs => {
+            if (docs.status == status) {
+                const li = `
+        <div class="item_container" title="Created on: ${formateDate(docs.createdDate)}">
+            <div data-id="${docs.id}" class="check-mark ${docs.status == "completed" ? "checked" : ""}">
+                <img src="images/icon-check.svg">
+            </div>
+            <div class="text-container">
+                <p class="task_name ${docs.status == "completed" ? "checked" : ""}">${docs.text}</p>
+                <div>
+                    <label class="task_category">${docs.category}</label> 
+                    <label class="task_endDate ${docs.endDate == "" ? "empty" : ""}"><i>Due in: ${timeLeft(docs.createdDate, docs.endDate)}</i></label>
+                </div>           
+            </div>
+          <div class="star-container ${docs.important == true ? "important" : ""}" data-id="${docs.id}" title="Mark as important">
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <div title="Delete task" data-id="${docs.id}" class="delete-text ${docs.status == "completed" ? "checked" : ""}">
+            <i class="fa-regular fa-trash-can fa-bounce fa-xl" style="color: #ff0000;"></i>
+          </div>
         </div>
-        <div class="text-container">
-            <p class="task_name ${docs.status == "completed" ? "checked" : ""}">${docs.text}</p>
-            <div>
-                <label class="task_category">${docs.category}</label> 
-                <label class="task_endDate"><i>Due in: 2hrs 3min</i></label>
-            </div>           
-        </div>
-      <div class="star-container ${docs.important == true ? "important" : ""}" data-id="${docs.id}" title="Mark as important">
-        <i class="fa-solid fa-star"></i>
-      </div>
-      <div title="Delete task" data-id="${docs.id}" class="delete-text ${docs.status == "completed" ? "checked" : ""}">
-        <i class="fa-regular fa-trash-can fa-bounce fa-xl" style="color: #ff0000;"></i>
-      </div>
-    </div>
-`;
-        container += li;
-    });
+    `;
+                container += li;
+            }
+        });
+    }
     return container;
+}
+
+function timeLeft(createdDate, endDate) {
+    const createDate = new Date(createdDate);
+    const futureDate = new Date(endDate);
+
+    const diffInMs = futureDate - createDate;
+
+    return convertTimeLeft(diffInMs);
+
+}
+
+function convertTimeLeft(milliseconds) {
+    let result;
+
+    const minutes = milliseconds / 60000;
+
+    if (minutes < 60) {
+        result = `${Math.floor(minutes)} minutes`;
+    } else if (minutes < 1440) {
+        const hours = Math.floor(minutes / 60);
+        const remainingMinutes = Math.floor(minutes % 60);
+        result = `${hours} hour${hours !== 1 ? 's' : ''}, ${remainingMinutes} minute${remainingMinutes !== 1 ? 's' : ''}`;
+    } else if (minutes < 525600) {
+        const days = Math.floor(minutes / 1440);
+        const remainingHours = Math.floor((minutes % 1440) / 60);
+        result = `${days} day${days !== 1 ? 's' : ''}, ${remainingHours} hour${remainingHours !== 1 ? 's' : ''}`;
+    } else {
+        const years = Math.floor(minutes / 525600);
+        const remainingMonths = Math.floor((minutes % 525600) / 43200);
+        result = `${years} year${years !== 1 ? 's' : ''}, ${remainingMonths} month${remainingMonths !== 1 ? 's' : ''}`;
+    }
+
+    return result;
 }
